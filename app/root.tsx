856aa0bfa1aa942610a7646a94eaf7220d4ea8af
Austin Atchley <austinatchley@gmail.com>
2024-04-27 18:39:47 -0400

    Add sakura.css

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/sakura.css/css/sakura.css"
+          type="text/css"
+        />
+
         <Meta />
         <Links />
       </head>
